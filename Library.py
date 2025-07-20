adfd587cdaa0ab1caa67f9dac09572dae3fd32ec
SM Nuruzzaman Nobel <103740303+Nobel712@users.noreply.github.com>
2025-07-20 13:10:06 +0600

    Create Library.py

---
+++ Library.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
+import os
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.python.keras.layers import Dense, Flatten
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+
+from tensorflow.keras.layers import Flatten, Dense, Dropout, BatchNormalization
+from tensorflow.keras.models import Model, Sequential
+
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import GlobalAveragePooling2D
+from tensorflow.keras import layers, models
